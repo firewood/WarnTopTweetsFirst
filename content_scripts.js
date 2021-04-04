f5e26476da42bc5bbb6335bc96278c71c9868d54
firewood <karamaki@gmail.com>
2021-04-05 00:41:14 +0900

    expand the height of More Tweets

--- content_scripts.js
+++ content_scripts.js
@@ -1,4 +1,4 @@
-setTimeout(function() {
+function warnTopTweetsFirst() {
   var h2 = document.getElementsByTagName('h2');
   for (let i = 0; i < h2.length; ++i) {
     var span = h2[i].getElementsByTagName('span')[0];
@@ -8,4 +8,29 @@ setTimeout(function() {
       span.style.color = 'red';
     }
   }
-}, 500);
+}
+
+function hideMoreTweets() {
+  if (!document.referrer) {
+    return;
+  }
+  let article = document.querySelector( 'div[data-testid="primaryColumn"] article[role="article"]' );
+  if (!article) {
+    return;
+  }
+  let articles = article.parentElement.parentElement.parentElement.parentElement;
+  if (!articles || !articles.children) {
+    return;
+  }
+
+  for (let i = 0; i < articles.children.length; ++i) {
+    let div = articles.children[i];
+    if (div.querySelector( 'h2[role="heading"]' )) {
+      div.style.height = '2000px';
+      break;
+    }
+  }
+}
+
+setTimeout(function() { warnTopTweetsFirst(); }, 500);
+setTimeout(function() { hideMoreTweets(); }, 3000);
