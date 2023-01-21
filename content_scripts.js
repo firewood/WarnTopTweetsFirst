fff23364bfa6e1c37833d4046ef65c22ebba6e44
firewood <karamaki@gmail.com>
2023-01-21 17:48:45 +0900

    1.0.3; adopt Twitter 2023 updates

--- content_scripts.js
+++ content_scripts.js
@@ -1,33 +1,30 @@
-function warnTopTweetsFirst() {
-  var h2 = document.getElementsByTagName('h2');
-  for (let i = 0; i < h2.length; ++i) {
-    var span = h2[i].getElementsByTagName('span')[0];
-    if (!span) continue;
-    if (['Home', 'ホーム'].includes(span.textContent)) {
-      span.style.fontSize = '30pt';
-      span.style.color = 'red';
+function chooseChronologicalOrder() {
+  const tabs = document.querySelectorAll('div[role="tablist"] a');
+  if (tabs.length == 2) {
+    const tab = tabs[1];
+    if (tab.ariaSelected === 'false') {
+      tab.click();
     }
   }
 }
 
 function hideMoreTweets() {
-    document.querySelectorAll('h2[role="heading"]').forEach((element, index) => {
-      if (element.textContent == 'その他のツイート' || element.textContent == '関連ツイート') {
-        element.textContent = ' ';
-        element.style.height = '2000px';
-      }
-    });
+  document.querySelectorAll('h2[role="heading"]').forEach((element, index) => {
+    if (element.textContent == 'その他のツイート' || element.textContent == '関連ツイート') {
+      element.textContent = ' ';
+      element.style.height = '2000px';
+    }
+  });
 }
 
 var count = 0;
-var check_timer = setInterval(check, 500);
+setTimeout(check, 500);
 
 function check() {
-  warnTopTweetsFirst();
+  chooseChronologicalOrder();
   hideMoreTweets();
 
-  if (++count >= 50) {
-    clearInterval(check_timer);
+  if (++count < 50) {
+    setTimeout(check, 500);
   }
 }
-
